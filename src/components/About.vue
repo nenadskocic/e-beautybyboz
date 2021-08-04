385ed406def0befd745fdd0a57cbec3b13a518ab
Nenad Skocic <nenad.skocic@gmail.com>
2021-08-04 13:08:46 -0400

    about -initial

---
+++ src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <div class="container">
+      <p>TESTTTTTTTTTTTTTTT</p>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+@import "@/assets/css/palette.scss";
+
+.about {
+  background-color: white;
+}
+</style>
