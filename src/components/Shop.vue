2f3f1f7b51640b461b0b668043b8a87c758f9a9d
Nenad Skocic <nenad.skocic@gmail.com>
2021-08-14 00:28:50 -0400

    shop -add tan products, grid layout

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,3 +1,85 @@
 <template>
-  <div class="shop"></div>
+  <div class="shop">
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>
+          <img
+            src="@/assets/images/products/tan/Venetian-Self-Tan-Mist-for-Face-2oz.png"
+          />
+          <div>
+            <p>Venetian Self Tan Mist for Face - <b>$14.00</b></p>
+            Fashioned after our award winning, professional Venetian Sunless
+            Solution. This exotic, micro-fine sunless mist for home use,
+            features a unique blend of violet and brown bronzers. Created to
+            mimic skin colors found on the beautiful beaches of the European
+            Riviera, this deliberate and innovative formulation allows color to
+            appear deeper, darker, and with no signs of an unnatural orange
+            color. <b>2 oz</b>
+          </div>
+        </b-col>
+        <b-col>
+          <img
+            src="@/assets/images/products/tan/Venetian-Self-Tan-Lotion-8.5oz.png"
+          />
+          <div>
+            <p>Venetian Self Tan Lotion- <b>$40.00</b></p>
+            An instant rapid, Venetian extended tan! Get an immediate Venetian
+            tan that will last up to 1 week. <b>8.50 oz</b>
+          </div>
+        </b-col>
+        <b-col>
+          <img
+            src="@/assets/images/products/tan/Venetian-Self-Tan-Mousse-8oz.png"
+          />
+          <div>
+            <p>Venetian Self Tan Mousse - <b>$42.00</b></p>
+            Developed from our award winning, professional Venetian Sunless
+            Solution. This exotic, lightweight, velvety self tan mousse for home
+            use, features a unique blend of violet and brown bronzers. Created
+            to mimic skin colors found on the beautiful beaches of the European
+            Riviera, this purposeful and ground-breaking formulation allows
+            color to appear deeper, darker, and with no signs of an unnatural
+            orange color. <b>8 oz</b>
+          </div>
+        </b-col>
+        <b-col>
+          <img
+            src="@/assets/images/products/tan/Venetian-Rapid-Self-Tan-Lotion.png"
+          />
+          <div>
+            <p>Venetian Rapid Self Tan Lotion - <b>$46.00</b></p>
+            An instant rapid, Venetian extended tan! Get an immediate Venetian
+            tan that will last up to 1 week.
+          </div>
+        </b-col>
+        <b-col sm="3">
+          <img
+            src="@/assets/images/products/tan/Venetian-Self-Tan-Mist-7oz.png"
+          />
+          <div>
+            <p>Venetian Self Tan Mist - <b>$54.00</b></p>
+            Fashioned after our award winning, professional Venetian Sunless
+            Solution. This exotic, micro-fine sunless mist for home use,
+            features a unique blend of violet and brown bronzers. Created to
+            mimic skin colors found on the beautiful beaches of the European
+            Riviera, this deliberate and innovative formulation allows color to
+            appear deeper, darker, and with no signs of an unnatural orange
+            color. <b>7 oz</b>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
+<script>
+</script>
+<style lang="scss">
+.container {
+  padding-bottom: 20px;
+}
+.bv-example-row img {
+  width: 300px;
+}
+
+@import "@/assets/css/palette.scss";
+</style>
