93ffedf32a6e838777d02730c9126a20fcc35dbc
Nenad Skocic <nenad.skocic@enigmadesign.io>
2021-08-24 12:00:56 -0400

    app -vue-mq plugin install

--- src/App.vue
+++ src/App.vue
@@ -16,19 +16,17 @@ import "./assets/css/palette.scss";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import { VueperSlides, VueperSlide } from "vueperslides";
-import "vueperslides/dist/vueperslides.css";
+import VueMq from 'vue-mq';
 
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
+Vue.use(VueMq, { breakpoints: { phone: 450, laptop: 1250, desktop: Infinity, }, defaultBreakpoint: 'phone'})
 
 export default {
   name: "App",
   components: {
     NavBar,
     Footer,
-    VueperSlides,
-    VueperSlide,
   },
 };
 </script>
