60787d555558f5671f33cbe0f1f52c7a6501044d
Nenad Skocic <nenad.skocic@gmail.com>
2021-09-27 10:30:33 -0400

    about -add BBB background info

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="about">
     <div class="container">
-      <p>TESTTTTTTTTTTTTTTT</p>
+      <p>
+        BeautybyBoz strives to provide clients with a welcoming environment
+        where individuals feel comfortable and confident in both the products
+        and services provided. With many years of experience in the customer
+        service industry we have received the opportunity to put together a team
+        that is positive, welcoming and creative. Our Team's values are to
+        provide clients with a service that is affordable yet does not alter the
+        quality of work and products that are being used.
+      </p>
+    </div>
+
+    <div class="container">
+      <p>
+        Our team is continually staying up to date on new products and
+        techniques in the beauty industry. We are here to provide you with a
+        comfortable and safe environment by adhering to the changing needs and
+        circumstances of our world.
+      </p>
     </div>
   </div>
 </template>
@@ -10,5 +27,9 @@
 
 .about {
   background-color: white;
+  font-size: 18px;
+}
+p {
+  margin: 0px;
 }
 </style>
