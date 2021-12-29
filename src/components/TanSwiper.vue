f7c508b8a3434778eab9628037b55b05e056f3c5
Nenad Skocic <nenad.skocic@gmail.com>
2021-12-29 15:39:17 -0500

    Added tanning products to swiper.

---
+++ src/components/TanSwiper.vue
@@ -0,0 +1,242 @@
+<template>
+  <swiper class="swiper" :options="swiperOption">
+    <swiper-slide>
+      <div class="container">
+        <div>
+          <img
+            src="@/assets/images/products/tan/Norvell_Sunless_Application_Mitt.png"
+          />
+        </div>
+        <div class="text">
+          <p>Norvell Applicator Mitt - <b>$8.00</b></p>
+          <br />
+          <p>
+            Use the applicator mitt for all self-tanning products. Easy to blend
+            and apply lotions, sprays, mousses, and serums to all areas of the
+            body.
+          </p>
+          <br /><b>Application:</b><br />
+          <ol>
+            <li>Apply self-tanning product according to directions.</li>
+            <li>Use mitt to apply and blend evenly.</li>
+            <li>
+              Care: Hand wash or machine wash gently with mild detergent. Let
+              air dry. Do not expose to direct heat.
+            </li>
+            <li>Do not bleach.</li>
+          </ol>
+        </div>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="container">
+        <div>
+          <img
+            src="@/assets/images/products/tan/Venetian_Self_Tanning_Mist_for_Face_2oz.png"
+          />
+        </div>
+        <div class="text">
+          <p>Venetian Self Tanning Mist for Face - <b>$14.00</b></p>
+          <br />
+
+          <p>
+            Fashioned after our award winning, professional Venetian Sunless
+            Solution. This exotic, micro-fine sunless mist for home use,
+            features a unique blend of violet and brown bronzers. Created to
+            mimic skin colors found on the beautiful beaches of the European
+            Riviera, this deliberate and innovative formulation allows color to
+            appear deeper, darker, and with no signs of an unnatural orange
+            color.
+          </p>
+          <br />
+          <p>Size: <b>2 fl oz</b></p>
+          <br /><b>Application:</b><br />
+          <ol>
+            <li>Lightly spray onto a make-up sponge.</li>
+            <li>
+              Gently blend on face and neck. Carefully blend into hairline.
+            </li>
+            <li>Wash hands after use.</li>
+          </ol>
+        </div>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="container">
+        <div>
+          <img
+            src="@/assets/images/products/tan/Venetian_Gradual_Self_Tanning_Lotion_8.5oz.png"
+          />
+        </div>
+        <div class="text">
+          <p>Venetian Gradual Self Tanning Lotion- <b>$40.00</b></p>
+          <br />
+          <p>
+            An instant rapid, Venetian extended tan! Get an immediate Venetian
+            tan that will last up to 1 week.
+          </p>
+          <br />
+          <p>Size: <b>8.5 fl oz</b></p>
+          <br /><b>Application:</b><br />
+          <ol>
+            <li>Smooth on evenly and blend thoroughly.</li>
+            <li>Use sparingly around knuckles, knees, ankles, and elbows.</li>
+            <li>
+              To avoid coloring of palms, use the Norvell Blending Mitt or wash
+              hands immediately with soap and water after application.
+            </li>
+          </ol>
+        </div>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="container">
+        <div>
+          <img
+            src="@/assets/images/products/tan/Venetian_Self_Tanning_Mousse_8oz.png"
+          />
+        </div>
+        <div class="text">
+          <p>Venetian Self Tanning Mousse - <b>$42.00</b></p>
+          <br />
+          <p>
+            Developed from our award winning, professional Venetian Sunless
+            Solution. This exotic, lightweight, velvety self tan mousse for home
+            use, features a unique blend of violet and brown bronzers. Created
+            to mimic skin colors found on the beautiful beaches of the European
+            Riviera, this purposeful and ground-breaking formulation allows
+            color to appear deeper, darker, and with no signs of an unnatural
+            orange color.
+          </p>
+          <br />
+          <p>Size: <b>8 fl oz</b></p>
+          <br /><b>Application:</b><br />
+          <ol>
+            <li>For best results, exfoliate 24-hours prior to application.</li>
+            <li>
+              Apply to clean, dry skin in a circular motion using the Norvell
+              Applicator Mit.t
+            </li>
+            <li>
+              Avoid over-application to elbows, hands, knees, ankles, and feet.
+            </li>
+            <li>Do not get wet for at least 8 hours after application.</li>
+          </ol>
+        </div>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="container">
+        <div>
+          <img
+            src="@/assets/images/products/tan/Venetian_Rapid_Self_Tanning_Lotion.png"
+          />
+        </div>
+        <div class="text">
+          <p>Venetian Rapid Self Tanning Lotion - <b>$46.00</b></p>
+          <br />
+          <p>
+            An instant rapid, Venetian extended tan! Get an immediate Venetian
+            tan that will last up to 1 week.
+          </p>
+          <br />
+          <p>Size: <b>8 fl oz</b></p>
+          <br /><b>Application:</b><br />
+          <ol>
+            <li>
+              To ensure an even tan, apply with the Norvell Applicator Mitt.
+            </li>
+            <li>
+              Smooth on evenly and blend thoroughly across areas to be tanned.
+            </li>
+            <li>
+              Avoid over application to elbows, hands, knees, ankles, and feet.
+            </li>
+            <li>Rinse at 1, 2 or 3 hours for a light, medium or dark tan.</li>
+          </ol>
+        </div>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="container">
+        <div>
+          <img
+            src="@/assets/images/products/tan/Venetian_Self_Tanning_Mist_7oz.png"
+          />
+        </div>
+        <div class="text">
+          <p>Venetian Self Tanning Mist - <b>$54.00</b></p>
+          <br />
+          <p>
+            Fashioned after our award winning, professional Venetian Sunless
+            Solution. This exotic, micro-fine sunless mist for home use,
+            features a unique blend of violet and brown bronzers. Created to
+            mimic skin colors found on the beautiful beaches of the European
+            Riviera, this deliberate and innovative formulation allows color to
+            appear deeper, darker, and with no signs of an unnatural orange
+            color.
+          </p>
+          <br />
+          <p>Size: <b>7.0 fl oz</b></p>
+          <br /><b>Application:</b><br />
+          <ol>
+            <li>For best results, exfoliate 24-hours prior to application.</li>
+            <li>
+              In a well-ventilated area, apply to clean, dry skin. Avoid
+              over-application to elbows, hands, knees, ankles, and feet. Do not
+              get wet for at least 8 hours after application.
+            </li>
+            <li>
+              Keep the can in constant motion and hold 10-12 inches from body.
+              Apply spray in a sweeping motion across area to be tanned.
+            </li>
+          </ol>
+        </div>
+      </div>
+    </swiper-slide>
+    <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div>
+  </swiper>
+</template>
+
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+export default {
+  name: "swiper-example-navigation",
+  title: "Navigation",
+  components: {
+    swiper,
+    swiperSlide,
+  },
+  data() {
+    return {
+      swiperOption: {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        slidesPerView: "auto",
+      },
+    };
+  },
+};
+</script>
+<style scoped>
+.swiper {
+  width: 100%;
+}
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.swiper-button-next {
+  color: black;
+}
+.swiper-button-prev {
+  color: black;
+}
+</style>
