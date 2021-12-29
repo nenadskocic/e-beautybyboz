1203192f5e642ef4d16791f8249080a6e6d128e9
Nenad Skocic <nenad.skocic@gmail.com>
2021-12-29 15:29:58 -0500

    Fix hyperlink accessibility, change background color.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,42 +7,56 @@
     <b-row>
       <b-navbar id="footerNav">
         <b-navbar-nav>
-          <b-nav-item :to="{ name: 'contact' }">Contact Us</b-nav-item>
+          <b-nav-item :to="{ name: 'contact' }"><p>Contact Us</p></b-nav-item>
           <b-nav-item>
             <div class="v1">|</div>
           </b-nav-item>
-          <b-nav-item>
-            <a href="https://www.instagram.com/beautybyboz/">Instagram</a>
+          <b-nav-item href="https://www.instagram.com/beautybyboz/">
+            Instagram
           </b-nav-item>
           <b-nav-item>
             <div class="v1">|</div>
           </b-nav-item>
-          <b-nav-item :to="{ name: 'about' }">About Us</b-nav-item>
+          <b-nav-item :to="{ name: 'about' }"><p>About Us</p></b-nav-item>
         </b-navbar-nav>
       </b-navbar>
     </b-row>
     <b-row>
       <p>©2021 BeautyByBoz</p>
     </b-row>
-    <b-row>
-      <p>Web design by <a href="http://www.enigmaly.ca">Enigmaly</a></p>
+    <b-row id="ref">
+      <p>Web design by <a href="https://www.enigmaly.ca">Enigmaly</a></p>
     </b-row>
   </footer>
 </template>
 <script></script>
-<style lang="scss">
+<style lang="scss" scoped>
 @import "@/assets/css/palette.scss";
 
 footer {
-  background-color: lightgray;
+  background-color: whitesmoke;
+  font-weight: 500;
+}
+footer a {
+  text-decoration: none;
+  color: gray !important;
+}
+footer a:hover {
+  text-decoration: none;
+  color: black !important;
+}
+footer p:hover {
+  color: black !important;
 }
 #top {
   font-size: 18px;
+  padding: 10px 0 10px 0;
 }
 .row {
   justify-content: center;
   text-align: center;
-  padding: 0.5%;
+  padding: 0;
+  margin: 0;
 }
 .row img {
   width: 150px;
@@ -54,8 +68,12 @@ footer {
   font-size: 18px;
 }
 #footerNav {
-  background-color: lightgray;
-  padding-top: 0;
-  height: 10%;
+  background-color: whitesmoke;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+}
+#ref {
+  padding: 10px 0 10px 0;
 }
 </style>
