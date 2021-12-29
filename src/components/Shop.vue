cb58d89545124ac8b589db2f0432f260fa974a4d
Nenad Skocic <nenad.skocic@gmail.com>
2021-12-29 15:38:59 -0500

    Moved swiper contents to TanSwiper.vue & TanSwiper imported.

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,85 +1,50 @@
 <template>
   <div class="shop">
-    <b-container class="bv-example-row">
-      <b-row>
-        <b-col>
-          <img
-            src="@/assets/images/products/tan/Venetian-Self-Tan-Mist-for-Face-2oz.png"
-          />
-          <div>
-            <p>Venetian Self Tan Mist for Face - <b>$14.00</b></p>
-            Fashioned after our award winning, professional Venetian Sunless
-            Solution. This exotic, micro-fine sunless mist for home use,
-            features a unique blend of violet and brown bronzers. Created to
-            mimic skin colors found on the beautiful beaches of the European
-            Riviera, this deliberate and innovative formulation allows color to
-            appear deeper, darker, and with no signs of an unnatural orange
-            color. <b>2 oz</b>
-          </div>
-        </b-col>
-        <b-col>
-          <img
-            src="@/assets/images/products/tan/Venetian-Self-Tan-Lotion-8.5oz.png"
-          />
-          <div>
-            <p>Venetian Self Tan Lotion- <b>$40.00</b></p>
-            An instant rapid, Venetian extended tan! Get an immediate Venetian
-            tan that will last up to 1 week. <b>8.50 oz</b>
-          </div>
-        </b-col>
-        <b-col>
-          <img
-            src="@/assets/images/products/tan/Venetian-Self-Tan-Mousse-8oz.png"
-          />
-          <div>
-            <p>Venetian Self Tan Mousse - <b>$42.00</b></p>
-            Developed from our award winning, professional Venetian Sunless
-            Solution. This exotic, lightweight, velvety self tan mousse for home
-            use, features a unique blend of violet and brown bronzers. Created
-            to mimic skin colors found on the beautiful beaches of the European
-            Riviera, this purposeful and ground-breaking formulation allows
-            color to appear deeper, darker, and with no signs of an unnatural
-            orange color. <b>8 oz</b>
-          </div>
-        </b-col>
-        <b-col>
-          <img
-            src="@/assets/images/products/tan/Venetian-Rapid-Self-Tan-Lotion.png"
-          />
-          <div>
-            <p>Venetian Rapid Self Tan Lotion - <b>$46.00</b></p>
-            An instant rapid, Venetian extended tan! Get an immediate Venetian
-            tan that will last up to 1 week.
-          </div>
-        </b-col>
-        <b-col sm="3">
-          <img
-            src="@/assets/images/products/tan/Venetian-Self-Tan-Mist-7oz.png"
-          />
-          <div>
-            <p>Venetian Self Tan Mist - <b>$54.00</b></p>
-            Fashioned after our award winning, professional Venetian Sunless
-            Solution. This exotic, micro-fine sunless mist for home use,
-            features a unique blend of violet and brown bronzers. Created to
-            mimic skin colors found on the beautiful beaches of the European
-            Riviera, this deliberate and innovative formulation allows color to
-            appear deeper, darker, and with no signs of an unnatural orange
-            color. <b>7 oz</b>
-          </div>
-        </b-col>
-      </b-row>
-    </b-container>
+    <div class="title-wrapper">
+      <div class="title-inner">
+        <h1>Tanning Products</h1>
+      </div>
+    </div>
+    <TanSwiper />
   </div>
 </template>
+
 <script>
+import TanSwiper from "./TanSwiper.vue";
+import Vue from "vue";
+Vue.use(TanSwiper);
+
+export default {
+  name: "Shop",
+  components: {
+    TanSwiper,
+  },
+};
 </script>
 <style lang="scss">
-.container {
-  padding-bottom: 20px;
+.shop {
+  height: calc(100vh - 459px);
+  flex-direction: column;
+  background-color: whitesmoke;
 }
 .bv-example-row img {
   width: 300px;
 }
-
+.title-wrapper {
+  width: 100%;
+  position: relative;
+  display: table;
+  table-layout: fixed;
+  //background-color: #dcc2b0;
+  background-color: rgb(236, 227, 221);
+  padding: 20px;
+}
+.title-inner {
+  text-align: center;
+}
+h1 {
+  font-family: "Source Sans Pro", sans-serif;
+  font-size: 32px;
+}
 @import "@/assets/css/palette.scss";
 </style>
