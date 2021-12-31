d39f2091aab6747bfb37b17ac4dfb4d759d057ec
Nenad Skocic <nenad.skocic@gmail.com>
2021-12-31 09:24:57 -0500

    Clean.

--- src/App.vue
+++ src/App.vue
@@ -8,38 +8,32 @@
 </template>
 
 <script>
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
+import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
 import Vue from "vue";
-import "./assets/css/palette.scss";
-
+import "./assets/css/styles.scss";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import VueMq from "vue-mq";
-import VueCarousel from "vue-carousel";
+import VueMq from 'vue-mq'
 import VueAwesomeSwiper from "vue-awesome-swiper";
 import "swiper/swiper-bundle.min.css";
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 Vue.use(VueMq, {
-  breakpoints: { phone: 450, laptop: 1250, desktop: Infinity },
-  defaultBreakpoint: "phone",
+  breakpoints: { phone: 450, tablet: 900, laptop: 1250, desktop: Infinity },
+  defaultBreakpoint: 'phone',
 });
-Vue.use(VueCarousel);
 Vue.use(VueAwesomeSwiper);
 
 export default {
-  name: "App",
+  name: "app",
   components: {
     NavBar,
     Footer,
   },
 };
 </script>
-
-<style>
-</style>
