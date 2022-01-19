5447f660337b9fdcb98f30275d0e76a841c06d6b
Nenad Skocic <nenad.skocic@gmail.com>
2022-01-18 22:38:02 -0500

    Navigation logic for non-mobile & mobile view implemented.

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,94 @@
-
 <template>
-  <div id="app">
-    <NavBar />
-    <router-view></router-view>
-    <Footer />
+  <div>
+    <NavigationMobile />
+    <div class="content" :class="{ open: showNav }">
+      <div class="top-bar">
+        <div id="navigation-icon" v-if="mobileView" @click="showNav = !showNav">
+          <i class="fas fa-bars"></i>
+        </div>
+        <Navigation v-if="!mobileView" />
+      </div>
+      <router-view></router-view>
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
-import NavBar from "./components/NavBar.vue";
+import Navigation from "./components/Navigation.vue";
+import NavigationMobile from "./components/NavigationMobile.vue";
 import Footer from "./components/Footer.vue";
+
 import Vue from "vue";
-import "./assets/css/styles.scss";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import VueMq from "vue-mq";
-import VueAwesomeSwiper from "vue-awesome-swiper";
-import "swiper/swiper-bundle.min.css";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "@fortawesome/fontawesome-free/js/all.js";
 
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
-Vue.use(VueMq, {
-  breakpoints: { mobile: 450, tablet: 900, laptop: 1250, desktop: Infinity },
-  defaultBreakpoint: "mobile",
-});
-Vue.use(VueAwesomeSwiper);
 
 export default {
-  name: "app",
+  data: () => {
+    return {
+      mobileView: true,
+      showNav: false,
+    };
+  },
+  methods: {
+    handleView() {
+      this.mobileView = window.innerWidth <= 990;
+    },
+  },
   components: {
-    NavBar,
+    Navigation,
+    NavigationMobile,
     Footer,
+  },
+  created() {
+    this.handleView();
+    window.addEventListener("resize", this.handleView);
   },
 };
 </script>
+
+<style lang="scss">
+@import url("https://use.fontawesome.com/releases/v5.9.0/css/all.css");
+* {
+  font-size: 1rem;
+  font-family: "Apple Chancery", cursive;
+}
+body {
+  width: 100%;
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  font-family: "Segoe UI", Tahoma;
+  background-color: whitesmoke;
+  display: flex;
+  flex-direction: column;
+}
+.top-bar {
+  display: flex;
+  width: 100%;
+}
+#navigation-icon {
+  padding: 10px 10px 20px;
+  margin-right: 10px;
+  cursor: pointer;
+  i {
+    font-size: 2rem;
+  }
+}
+.content {
+  position: absolute;
+  top: 0px;
+  width: calc(100%);
+  min-height: calc(100vh);
+  padding: 20px;
+  background-color: rgb(175, 174, 174);
+  transition: 1s transform cubic-bezier(0, 0.12, 0.14, 1);
+}
+.open {
+  transform: translateX(300px);
+}
+</style>
