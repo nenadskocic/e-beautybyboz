2ecc05057c515e6a49718172ac3b8fde05da9a49
Nenad Skocic <nenad.skocic@gmail.com>
2022-01-18 22:33:45 -0500

    3D carousel swiper component created.

---
+++ src/components/PortfolioSwiper.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="swiper-3d">
+    <carousel-3d>
+      <slide :index="0">
+        <img
+          src="../assets/images/stock/portrait_stock_bw.jpg"
+          height="300px"
+          width="300px"
+        />
+      </slide>
+      <slide :index="1">
+        <img
+          src="../assets/images/stock/portrait_stock_bw.jpg"
+          height="300px"
+          width="300px"
+        />
+      </slide>
+      <slide :index="2">
+        <img
+          src="../assets/images/stock/portrait_stock_bw.jpg"
+          height="300px"
+          width="300px"
+        />
+      </slide>
+      <slide :index="3">
+        <img
+          src="../assets/images/stock/portrait_stock_bw.jpg"
+          height="300px"
+          width="300px"
+        />
+      </slide>
+      <slide :index="4">
+        <img
+          src="../assets/images/stock/portrait_stock_bw.jpg"
+          height="300px"
+          width="300px"
+        />
+      </slide>
+    </carousel-3d>
+  </div>
+</template>
+
+<script>
+import { Carousel3d, Slide } from "vue-carousel-3d";
+
+export default {
+  name: "swiper-example-navigation",
+  title: "Navigation",
+  components: {
+    Carousel3d,
+    Slide,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+.swiper-3d {
+  margin-bottom: 4rem;
+}
+</style>
